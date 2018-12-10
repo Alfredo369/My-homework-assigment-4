6ddd17db03acd816d3da8a190f2edfa02790f9fd
Alfredo369 <45565835+Alfredo369@users.noreply.github.com>
2018-12-10 08:39:17 -0600

    Add files via upload

---
+++ hmwk_4/hmwk_4.py
@@ -0,0 +1,47 @@
+# problem 2-1
+message = "Hello to whom ever is reading this"
+print(message)
+
+# problem 2-2
+message = "This is problem 2-2"
+print(message)
+
+# problem 2-3
+first_name = "alfredo"
+last_name = 'nino'
+full_name = first_name + " " + last_name
+print(full_name)
+message = 'Hello, ' + full_name.title() + '!'' ' "Would you like to learn some Python today?"
+print(message)
+
+# problem 2-4
+first_name = "alfredo"
+last_name = 'nino'
+full_name = first_name + " " + last_name
+
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+# problem 2-5
+quote = 'Ben Parker once said, "Great power comes with great responsibilities."'
+print(quote)
+# problem 2-6
+famous_person = 'William Shakespeare'
+massage_1 = ' once said, "Frailty, thy name is woman!"'
+print(famous_person + massage_1)
+# problem 2-7
+name = '\t' 'Bob ' '\n' '\t' "flanders"
+print(name)
+favorite_language = 'python '
+favorite_language.rsplit()
+# problem 2-8
+print(2 + 6)
+print(4 * 2)
+print(10 - 2)
+print(16 / 2)
+# problem 2-9
+favorite_number = 118
+message = 'your favorite number is ' + str(favorite_number)
+print(message)
+# problem 2-10
+# Alfredo Nino homework 4 class ID 18
